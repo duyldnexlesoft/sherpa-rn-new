4ffca36ead5032c6cb57bca83e96ea82b78f070c
duyld <duyld@milemarker.io>
2025-05-17 22:30:24 +0700

    update

--- app/components/TextInput.tsx
+++ app/components/TextInput.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react';
-import { View, TextInput as TextInputReact, Pressable, TextInputProps } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, TextInput as TextInputReact, Pressable, TextInputProps} from 'react-native';
 // import TextInputMask from 'react-native-text-input-mask';
-import { Controller } from 'react-hook-form';
-import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import {Controller} from 'react-hook-form';
+import Animated, {useAnimatedStyle, useSharedValue, withTiming} from 'react-native-reanimated';
 
 export interface InputProps extends TextInputProps {
   name?: any;
@@ -32,7 +32,7 @@ const PADDING_LEFT = 10;
 const DURATION = 200;
 
 const InputComponent = (props: any) => {
-  const field = props.field;  
+  const field = props.field;
   const [lableInput, setLableInput] = useState(false);
   const [focusInput, setFocusInput] = useState(false);
   const [widthLeftIcon, setWidthLeftIcon] = useState(PADDING_LEFT);
@@ -40,7 +40,7 @@ const InputComponent = (props: any) => {
   const font = useSharedValue(field?.value ? FONT_ACTIVE : FONT_NOACTIVE);
   const left = useSharedValue(widthLeftIcon);
   const styleTop = useAnimatedStyle(() => ({
-    transform: [{ translateY: top.value }],
+    transform: [{translateY: top.value}],
     marginLeft: left.value,
     fontSize: font.value,
   }));
@@ -50,9 +50,9 @@ const InputComponent = (props: any) => {
   }, [widthLeftIcon]);
 
   useEffect(() => {
-    top.value = withTiming(lableInput ? TOP_NOACTIVE : TOP_ACTIVE, { duration: DURATION });
-    font.value = withTiming(lableInput ? FONT_ACTIVE : FONT_NOACTIVE, { duration: DURATION });
-    left.value = withTiming(lableInput ? PADDING_LEFT : widthLeftIcon, { duration: DURATION });
+    top.value = withTiming(lableInput ? TOP_NOACTIVE : TOP_ACTIVE, {duration: DURATION});
+    font.value = withTiming(lableInput ? FONT_ACTIVE : FONT_NOACTIVE, {duration: DURATION});
+    left.value = withTiming(lableInput ? PADDING_LEFT : widthLeftIcon, {duration: DURATION});
   }, [lableInput]);
 
   useEffect(() => {
@@ -83,32 +83,24 @@ const InputComponent = (props: any) => {
     }
   };
   return (
-    <View
-      className={
-        'w-full flex-row items-center justify-between rounded-md border bg-white ' + cssBorder()
-      }
-      style={{ height: props.height || 50 }}>
+    <View className={'w-full flex-row items-center justify-between rounded-md border bg-white ' + cssBorder()} style={{height: props.height || 50}}>
       {props.onPress && <Pressable className="absolute z-20 h-12 w-full" onPress={props.onPress} />}
       {props.leftIcon && (
         <Pressable
-          onLayout={(event) =>
-            setWidthLeftIcon((event.nativeEvent.layout.width || 0) + PADDING_LEFT)
-          }
+          onLayout={event => setWidthLeftIcon((event.nativeEvent.layout.width || 0) + PADDING_LEFT)}
           className={`flex h-12 items-center justify-center p-2 ${props.leftAction ? 'z-30' : ''}`}
           onPress={props.leftAction}>
           {props.leftIcon}
         </Pressable>
       )}
       {props.label && (
-        <Animated.Text
-          className='absolute top-4 rounded-md bg-white px-2 text-gray-500'
-          style={[styleTop, { fontFamily: 'Ubuntu' }]}>
+        <Animated.Text className="absolute top-4 rounded-md bg-white px-2 text-gray-500" style={[styleTop, {fontFamily: 'Ubuntu'}]}>
           {props.label}
         </Animated.Text>
       )}
       {/* {!props.mask && ( */}
       <TextInputReact
-        className="h-[90%] flex-1 pl-3 text-textContainer"
+        // className="h-[90%] flex-1 pl-3 text-textContainer"
         onEndEditing={handleOnEndEditing}
         onFocus={handleOnFocus}
         onBlur={field?.onBlur || props?.onBlur}
@@ -121,7 +113,8 @@ const InputComponent = (props: any) => {
         autoCorrect={false}
         ref={props.comRef}
         {...props}
-        style={[{ fontFamily: 'Ubuntu' }, props.style]}
+        className={`h-[90%] flex-1 pl-3 text-textContainer ${props.className}`}
+        style={[{fontFamily: 'Ubuntu'}, props.style]}
       />
       {/* )} */}
       {/* {props.mask && (
@@ -142,9 +135,7 @@ const InputComponent = (props: any) => {
         />
       )} */}
       {props.rightIcon && (
-        <Pressable
-          className={`flex h-12 items-center justify-center p-2 ${props.rightAction ? 'z-30' : ''}`}
-          onPress={props.rightAction}>
+        <Pressable className={`flex h-12 items-center justify-center p-2 ${props.rightAction ? 'z-30' : ''}`} onPress={props.rightAction}>
           {props.rightIcon}
         </Pressable>
       )}
@@ -154,12 +145,7 @@ const InputComponent = (props: any) => {
 
 const TextInput = (props: InputProps) => {
   return props.control ? (
-    <Controller
-      control={props.control}
-      rules={props.rules}
-      render={({ field }) => <InputComponent field={field} {...props} />}
-      name={props.name}
-    />
+    <Controller control={props.control} rules={props.rules} render={({field}) => <InputComponent field={field} {...props} />} name={props.name} />
   ) : (
     <InputComponent {...props} />
   );
