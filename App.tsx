358661e117a47da5e3e089b0684b6b2465f6ce08
duyld <duyld@milemarker.io>
2025-05-17 10:36:25 +0700

    update

--- App.tsx
+++ App.tsx
@@ -2,10 +2,8 @@
 import React, {useEffect} from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import AppNavigator from './app/navigation/AppNavigator';
-import { enableScreens } from 'react-native-screens';
-
 import {SENTRY_DSN, MAPBOX_ACCESS_TOKEN, STRIPE_PUBLISHABLE_KEY} from '@env';
-// import * as Sentry from '@sentry/react-native';
+import * as Sentry from '@sentry/react-native';
 import {ErrorBoundary} from './app/errors/ErrorBoundary';
 import {Provider} from 'react-redux';
 import store from './app/store/store';
@@ -13,45 +11,45 @@ import './reactotronConfig';
 import 'locales/i18next';
 import './global.css';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-// import {StripeProvider} from '@stripe/stripe-react-native';
-// import Mapbox from '@rnmapbox/maps';
+import {StripeProvider} from '@stripe/stripe-react-native';
+import Mapbox from '@rnmapbox/maps';
 import {Dimensions, StatusBar} from 'react-native';
 let deviceHeight = Dimensions.get('screen').height;
 let windowHeight = Dimensions.get('window').height;
-// Mapbox.setAccessToken(MAPBOX_ACCESS_TOKEN);
+Mapbox.setAccessToken(MAPBOX_ACCESS_TOKEN);
 
-// if (SENTRY_DSN) {
-//   Sentry.init({
-//     dsn: SENTRY_DSN,
-//     enableAppHangTracking: false,
-//   });
-// }
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    enableAppHangTracking: false,
+  });
+}
 
 const queryClient = new QueryClient();
 
 function App(): React.JSX.Element {
   const navHeight = deviceHeight - windowHeight - (StatusBar.currentHeight || 0);
-  // useEffect(() => {
-  //   Mapbox.setTelemetryEnabled(false);
-  //   Mapbox.Logger.setLogCallback(log => {
-  //     if (log.message.includes('RNMBX-mapview-callouts_drag')) {
-  //       return true;
-  //     }
-  //     if (log.message.includes('RNMBX-mapview-point-annotations_drag')) {
-  //       return true;
-  //     }
-  //     return false;
-  //   });
-  // }, []);
+  useEffect(() => {
+    Mapbox.setTelemetryEnabled(false);
+    Mapbox.Logger.setLogCallback(log => {
+      if (log.message.includes('RNMBX-mapview-callouts_drag')) {
+        return true;
+      }
+      if (log.message.includes('RNMBX-mapview-point-annotations_drag')) {
+        return true;
+      }
+      return false;
+    });
+  }, []);
 
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <GestureHandlerRootView style={{flex: 1, paddingBottom: navHeight > 20 ? navHeight : 0}}>
           <ErrorBoundary>
-            {/* <StripeProvider publishableKey={'STRIPE_PUBLISHABLE_KEY'}> */}
+            <StripeProvider publishableKey={STRIPE_PUBLISHABLE_KEY}>
               <AppNavigator />
-            {/* </StripeProvider> */}
+            </StripeProvider>
           </ErrorBoundary>
         </GestureHandlerRootView>
       </QueryClientProvider>
